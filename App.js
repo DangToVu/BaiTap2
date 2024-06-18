cc74108a33621c76522f6f959992aeee8948d3d3
DangToVu <vuminhdang811@gmail.com>
2024-06-18 22:53:29 +0700

    BaiTap2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,Button} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>A button that alert "Hello"!</Text>
+      <Button color='orange'
+              title='Button' 
+              onPress={() => alert("Hello!")}/>
       <StatusBar style="auto" />
     </View>
   );
